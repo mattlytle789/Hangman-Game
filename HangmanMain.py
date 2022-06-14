6221666ae55bd754ecd0734a28075b1812750f85
Matthew Lytle <mbl31@pitt.edu>
2022-06-14 16:12:19 -0400

    Started main file

---
+++ HangmanMain.py
@@ -0,0 +1,27 @@
+## Hangman Game
+
+from random_word import RandomWords
+
+# creating an instance of the RandomWords object
+randomWord = RandomWords()
+
+# Printing a welcome scree to the player
+print(" ____________________________________________________________________")
+print("|                                                                    |")
+print("|                                                                    |")
+print("|                                                                    |")
+print("|                        Welcome to Hangman                          |")
+print("|                                                                    |")
+print("|                                                                    |")
+print("|                       press enter to start                         |")
+print("|                                                                    |")
+print("|                                                                    |")
+print("|                                                                    |")
+print("|____________________________________________________________________|")
+# waiting for the user to press enter
+input('')
+
+
+
+# printing Game over
+print("Game Over")
